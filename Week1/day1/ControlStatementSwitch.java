f1aa158386e8b474435cab0af6adeb14c12d817a
Jothi1985 <127149773+Jothi1985@users.noreply.github.com>
2023-08-20 09:36:27 +0200

    Add files via upload

---
+++ Week1/day1/ControlStatementSwitch.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class ControlStatementSwitch {
+	public static void main(String args[]) {
+		int num = 2;
+		switch(num) {
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+		}
+	}
+
+}
