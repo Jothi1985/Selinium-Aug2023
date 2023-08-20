896621d593f5bda96fdf620fb7e8b2274ed6d9d1
Jothi1985 <127149773+Jothi1985@users.noreply.github.com>
2023-08-20 09:09:53 +0200

    Create Informations.java

---
+++ Week1/day1/Informations.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Informations 
+{
+	public static void main(String[] args)
+	{
+		int number = 20;
+		float mile= 20.5f;
+		boolean state= true;
+		long creditnumber = 1234567890l;
+		double salary = 23456.3444;
+		char letter ='H';
+		String name ="Jothi";
+		System.out.println("number : "+ number);
+		System.out.println("mile :"+ mile);
+		System.out.println("state :"+ state);
+		System.out.println("creditnumber :"+ creditnumber);
+		System.out.println("salary :"+ salary);
+		System.out.println("letter :"+ letter);
+		System.out.println("name : "+ name);
+	}
+}
