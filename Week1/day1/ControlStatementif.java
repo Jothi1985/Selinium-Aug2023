e290c33600966d50e10c8f462ecd495c3fb23e1a
Jothi1985 <127149773+Jothi1985@users.noreply.github.com>
2023-08-20 09:29:45 +0200

    Add files via upload

---
+++ Week1/day1/ControlStatementif.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class ControlStatementif {
+	
+	public static void main(String[] args)
+	{
+		String lang = "Tamil";
+				if (lang== "Tamil") 
+				{System.out.println("country is tamilnadu + lang is :" +lang);}
+				else
+				{System.out.println("country is other:"+lang);}
+				
+	}
+
+}
