0cff964a3ab7e0caeda1739c6de6d3d3d106563b
Jothi1985 <127149773+Jothi1985@users.noreply.github.com>
2023-08-21 20:07:41 +0200

    Add files via upload

---
+++ day2/Calculator.java
@@ -0,0 +1,22 @@
+package week1.day2;
+
+public class Calculator
+{
+	
+	private int addTwoNumbers(int num1,int num2)
+	{
+		return num1+num2;
+	}
+
+	public float mul(float num3,float num4)
+	{
+		return num3 * num4;
+	}
+	public void sub()
+	
+	{    int a =10;int b = 5;
+	    
+		System.out.println(a-b);
+		 
+	}
+}
---
+++ day2/OddNumbers.java
@@ -0,0 +1,17 @@
+package week1.day2;
+//to check odd/even number
+
+public class OddNumbers {
+
+	public static void main(String args[])
+	{
+		int n = 10;
+		for(int i=1;i<=n;i++)
+		{
+			if(i%2!=0) 
+				// i%2 not equals to zero, the number is odd  
+			{System.out.println(i);}
+		}
+
+}
+	}
